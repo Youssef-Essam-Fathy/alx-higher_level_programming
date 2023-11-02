121de6b39e484e23cc88a6a90a0928456a21c291
Youssef <youssef@email.com>
2023-11-02 15:53:36 +0200

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    sum, minus, multip, quot = add(a,b), sub(a,b), mul(a,b), div(a,b)
+    print("{} + {} = {}".format(a, b, sum))
+    print("{} - {} = {}".format(a, b, minus))
+    print("{} * {} = {}".format(a, b, multip))
+    print("{} / {} = {}".format(a, b, quot))
