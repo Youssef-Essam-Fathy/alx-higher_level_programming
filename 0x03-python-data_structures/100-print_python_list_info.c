cc0ca43ea7443c27ca3c502ff2dc487df35c2dd1
Youssef <youssef@email.com>
2023-11-06 19:29:08 +0200

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: the python object
+ * Return: void
+ **/
+void print_python_list_info(PyObject *p)
+{
+
+}
