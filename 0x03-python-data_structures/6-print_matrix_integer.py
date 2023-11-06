8befa54cb29e6a5ff4242e60e4149fe6d9899e88
Youssef <youssef@email.com>
2023-11-06 21:44:54 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print("{}".format(matrix))
+    if not matrix:
+        return None
+    for submatt in matrix:
+        if len(submatt) == 0:
+            print()
+        for idx in range(len(submatt)):
+            print("{:d}".format(submatt[idx]),
+                  end="\n" if idx is len(submatt) - 1 else " ")
