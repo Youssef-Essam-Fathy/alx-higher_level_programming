5c9bcc5f7310681daccdb8a9f788acc521624224
Youssef <youssef@email.com>
2023-11-09 03:54:49 +0200

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    ttl = 0
+    nm = 0
+    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        nm = dictionary[i]
+        ttl += nm if ttl < nm * 5 else -nm
+    return ttl
