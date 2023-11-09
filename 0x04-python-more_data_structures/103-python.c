aed4f4fb645a77c4b1080a1c8ea879f8ea26747b
Youssef <youssef@email.com>
2023-11-09 04:44:22 +0200

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,39 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+
+/**
+ * print_python_list - print list info
+ * @p: pointer
+ * Return: void
+ **/
+void print_python_list(PyObject *p)
+{
+	int range, putt, idx;
+	const char *kind;
+	PyListObject *lista = (PyListObject *)p;
+	PyVarObject *vare = (PyVarObject *)p;
+
+	range =  vare->ob_size;
+	putt = lista->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", range);
+	printf("[*] Allocated = %d\n", putt);
+
+	for (idx = 0; idx < range; idx++)
+	{
+		kind = lista->ob_item[idx]->ob_type->tp_name;
+		printf("Element %d: %s\n", idx, kind);
+		if (strcmp(kind, "bytes") == 0)
+			print_python_bytes(lista->ob_item[idx]);
+	}
+}
+
+/**
+ * print_python_bytes - print basic info about python
+ **/
+void print_python_bytes(PyObject *p)
+{}
