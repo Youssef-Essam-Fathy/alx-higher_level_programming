08016106eda1c5516703ecb959486af9cbe4aed7
Youssef <youssef@email.com>
2023-11-21 00:36:04 +0200

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    is_integer = True
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        is_integer = False
+    return is_integer
