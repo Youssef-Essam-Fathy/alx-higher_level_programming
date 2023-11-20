a10fdaf2845c6ad7e3bedb0f6af4a017c8bb0e9d
Youssef <youssef@email.com>
2023-11-20 23:39:02 +0200

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    try:
+        while idx is not x:
+            print(mylist[i], end='')
+            idx += 1
+    except IndexError:
+        None
+    print()
+    return idx 
