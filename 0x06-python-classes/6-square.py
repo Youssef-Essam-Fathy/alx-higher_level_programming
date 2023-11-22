9c47556f430aedb8648e723c7a1e83a077bbddfa
Youssef <youssef@email.com>
2023-11-22 04:03:14 +0200

    0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,83 +1,90 @@
 #!/usr/bin/python3
-"""
-No module imported
-"""
+"""Defines a class Square"""
 
 
 class Square:
     """
-    Private instance attribute size
-    public instance method
-    """
+    Class that defines properties of square by: (based on 5-square.py).
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """private instance attribute
-        parameters
-        -------------------------
-        size : integer else TypeError
-        if size less than 0, raise value error
+        """Creates new instances of square.
+
+        Args:
+            __size (int): size of the square (1 side).
+            __position (tuple): position of the square.
         """
         self.size = size
         self.position = position
 
-    @property
-    def size(self):
-        """
-        to retrieve private instance attribute size
-        """
-        return self.__size
-
-    @property
-    def position(self):
-        """to retrieve private instance attribute position"""
-        return self.__position
-
-    @size.setter
-    def size(self, value):
-        """
-        to set private instance attribute
-        """
-        self.__size = value
-        try:
-            assert type(value) == int
-        except BaseException:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-
-    @position.setter
-    def position(self, value):
-        """to set position, a tuple of two integers"""
-        self.__position = value
-        try:
-            assert type(value) == tuple
-        except BaseException:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        try:
-            assert type(value[0]) == int or type(value[1]) == int
-        except BaseException:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
     def area(self):
-        """
-        public instance method
-        returns the current square area
+        """Calculates the area of square.
+
+        Returns: the current square area.
         """
         return self.__size ** 2
 
+    @property
+    def size(self):
+        """Returns the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: size must be >= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Returns the position of the square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Property setter for position.
+
+        Args:
+            value (tuple): position of the square.
+
+        Raises:
+            TypeError: position must be a tuple of 2 positive integers
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def my_print(self):
+        """prints in stdout the square with the character #
         """
-        print squre using #
-        """
-        if self.size == 0:
+
+        if self.__size == 0:
             print()
-        for i in range(self.position[1]):
-            print("\n")
-        for i in range(self.size):
-            for j in range(self.position[0]):
-                print(" ", end="")
-            for j in range(self.size):
-                print("#", end="")
-            print()
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ",  end="")
+                print("#" * (self.__size))
