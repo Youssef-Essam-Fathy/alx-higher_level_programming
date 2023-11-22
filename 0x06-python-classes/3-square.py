921aaff1fdc05874d67d485b2b0a0e1cd20e608b
Youssef <youssef@email.com>
2023-11-22 03:05:16 +0200

    0x06-python-classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -24,9 +24,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+
         """ Public instance method
         Returns:
             The size of square
         """
+
         self.__size
