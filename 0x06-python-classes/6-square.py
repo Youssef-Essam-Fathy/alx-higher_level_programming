95897a7458c27af388a09ea645d9aeb5439ea35f
Youssef <youssef@email.com>
2023-11-22 04:04:11 +0200

    0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,9 +82,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for j in range(self.__position[1]):
+            for jdx in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
-                for k in range(self.__position[0]):
+            for idx in range(self.__size):
+                for kdx in range(self.__position[0]):
                     print(" ",  end="")
                 print("#" * (self.__size))
