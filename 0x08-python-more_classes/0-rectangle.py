9d2c7808841c6afe58f0ced1aae965fda9cc09e9
Youssef <youssef@email.com>
2023-11-27 21:45:26 +0200

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""No module importing"""
+
+class Rectangle:
+    """Empty Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Define a rectangle"""
+    def __init__(self, width=0, height=0):
+        """
+        Constructor
+        Args:
+            width: the ractangle width
+            height: the rectangle height
+        """
+        self.__width = width
+        self.__height = height
