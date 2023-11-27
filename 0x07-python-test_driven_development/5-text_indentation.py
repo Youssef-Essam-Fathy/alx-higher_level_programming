66a98403fbfb66debe386167593c303bae7db61b
Youssef <youssef@email.com>
2023-11-27 19:51:13 +0200

    0x08-python-more_classes

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to print a text with 2 new lines
+after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """
+    Method for adding 2 new lines after '.?:' chars
+
+    args:
+        text: the str text
+
+    Raises:
+        TypeError: If text is not a str
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".?:":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
