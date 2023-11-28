9c4460fc99f79d537f40c1b8466ff23c6c4f10c3
Youssef <youssef@email.com>
2023-11-29 00:51:31 +0200

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -19,3 +19,9 @@ def text_indentation(text):
     for delim in ".?:":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
