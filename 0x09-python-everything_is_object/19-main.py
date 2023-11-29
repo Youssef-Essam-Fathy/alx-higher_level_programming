e01df55f116b24fc17a75883c906022a6db05b85
Youssef <youssef@email.com>
2023-11-29 22:41:08 +0200

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-copy_list = __import__('19-copy_list').copy_list
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
+a = (1)
+b = (1)
+a is b
