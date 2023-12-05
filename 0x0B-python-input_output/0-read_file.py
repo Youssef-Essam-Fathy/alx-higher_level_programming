6f15dcde2c03a2226d332df2294327db2e6e0575
Youssef <youssef@email.com>
2023-12-05 22:49:58 +0200

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,4 +3,5 @@
 
 def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as file:
-        file.read(filename)
+        file_read = file.read(filename)
+        print(file_read)
