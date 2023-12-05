fd941f8f5792db87a6b7dee588b5794b6aa0b5d4
Youssef <youssef@email.com>
2023-12-05 22:52:12 +0200

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-
+"""No module importing"""
 
 def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8)
+    and prints it to stdout
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         file_read = file.read(filename)
-    print(file_read)
+        print(file_read)
