9cb42061c9c2b05de8dc844d6b25e658a40e678d
Youssef <youssef@email.com>
2023-12-06 00:57:50 +0200

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""No module importing"""
+
+
+def write_file(filename="", text=""):
+    """
+    a function that writes a text file (UTF8)
+    and returns the number of characters written
+    args:
+        filename: the  name of the file to write to
+        text: the text to be written in the file
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return file.tell()
