fea6c89d0769c9f49cd3aa27640820d88cd92837
youssef_esssam_fathy <youssefessam5623@gmail.com>
2023-12-06 11:01:32 -0800

    0x0B-python-input_output

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+"""No module importing"""
+
+def lookup(obj):
+    """a function that returns the list of 
+       available attributes and methods of an object"""
+    return list(obj)
