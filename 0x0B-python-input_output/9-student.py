429cf765fbc19e6616a59466266bcdc547ba2f87
Youssef <youssef@email.com>
2023-12-06 05:01:56 +0200

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""No module importing"""
+
+
+class Student:
+    """representation of a class"""
+    def __init__(self, first_name, last_name, age):
+    """Instantiation of a class"""
+    self.fr_name = first_name
+    self.ls_name = last_name
+    self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
