c4277f130e46b8e296fe9da5354fe1ab80331086
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-11 23:06:37 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 ''' No module importing '''
+
+
 class Base:
+    ''' a representation for our oop '''
+    
     __nb_objects = 0
 
     def __init__(self, id=None):
-        
+        '''Constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
