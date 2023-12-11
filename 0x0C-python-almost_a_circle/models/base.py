4c146c340d1beac3763b60c43e572758f786472a
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-11 23:19:24 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-''' No module importing '''
+'''No module importing'''
 
 
 class Base:
-    ''' a representation for our oop '''
-    
+    '''a representation for our oop'''
+
     __nb_objects = 0
 
     def __init__(self, id=None):
