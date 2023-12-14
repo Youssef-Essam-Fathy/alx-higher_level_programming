16a58a1aed47ae9123b81b75e328881c004603f9
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-15 00:15:06 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,6 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if self.__y < 0:
             raise ValueError("y must be >= 0")
+    
+    def area(self):
+        return self.width * self.height
