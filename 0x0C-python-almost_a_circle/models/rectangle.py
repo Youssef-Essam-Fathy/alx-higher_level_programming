4efab461628b54995b3e9214af673a874aa590a7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-15 17:51:27 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''importing Base class module'''
 
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -80,3 +80,16 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
             f"{self.width}/{self.height}"
         )
+
+    def update(self, *args):
+        '''assigns an argument to each attribute'''
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
