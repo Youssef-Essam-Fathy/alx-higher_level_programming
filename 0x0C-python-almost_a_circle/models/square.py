4914fcc6e7a1ba2b2a1a1a47ffbb13d82a5380df
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-15 20:46:36 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,4 +5,40 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    ''''''
+    '''A class representation area'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor'''
+        super().__init__(id, x, y, size, size)
+
+    @property
+    def size(self):
+        '''Getter for the size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''Setter for the size'''
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        '''Return a string representation of the square instance'''
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y} - "
+            f"{self.size}"
+        )
+
+    def update(self, *args, **kwargs):
+        '''assigns an argument to each attribute'''
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.x = args[1]
+            if len(args) >= 3:
+                self.y = args[2]
+            if len(args) >= 4:
+                self.size = args[3]
+
+        for key, value in kwargs.items():
+            setatrr(self, key, value)
