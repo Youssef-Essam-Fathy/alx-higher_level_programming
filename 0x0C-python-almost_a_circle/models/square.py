b44a0df88c576d1deffa7ce9aa09711622757de3
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-15 21:23:22 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,11 +34,11 @@ class Square(Rectangle):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.x = args[1]
+                self.size = args[1]
             if len(args) >= 3:
-                self.y = args[2]
+                self.x = args[2]
             if len(args) >= 4:
-                self.size = args[3]
+                self.y = args[3]
 
         for key, value in kwargs.items():
             setatrr(self, key, value)
