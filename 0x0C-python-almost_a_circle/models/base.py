df6e502c2b397f8b28b375e5c0e02693e2c70c58
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2023-12-16 00:10:45 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,11 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    def from_json_string(json_string):
+        if json_string is None:
+            return "[]"
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         if list_objs is None:
