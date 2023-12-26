e62c1211b139cd206baa3d9e9321c834ef86b7f0
Youssef-Essam-Fathy <youssefessam5623@email.com>
2023-12-26 07:13:02 +0200

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""No module importing"""
+
+
+class BaseGeometry():
+    '''class'''
+    def area(self):
+        '''Constructor'''
+        raise Exception("area() is not implemented")
