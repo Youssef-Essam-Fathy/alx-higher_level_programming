0f799aea2b930da3572c3b8e23fc827238ec4fd7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-17 04:48:33 +0200

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
