752f92466f3ff87497f8bca8a45136511efb63e7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 14:02:26 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS states(`id` INT PRIMARY KEY UNIQUE(ID) AUTO_INCREMENT NOT NULL, `name` VARCHAR(256) NOT NULL);
+CREATE table IF NOT EXISTS states(`id` INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL, `name` VARCHAR(256) NOT NULL);
