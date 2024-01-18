f0ba06ee342218981b8daa4b48512da57d7249a8
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 13:21:45 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user user_0d_1
-IF 'user_0d_1' NOT EXISTS
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
