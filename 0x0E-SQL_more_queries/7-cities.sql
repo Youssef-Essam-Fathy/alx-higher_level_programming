dc70a5394fa44de0e25886d0e6b0839ba8fd9f0a
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 16:08:42 +0200

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+CREATE table IF NOT EXISTS hbtn_0d_usa.cities
+(
+`id` INT PRIMARY KEY UNIQUE NOT NULL,
+`name` VARCHAR(256) NOT NULL,
+`state_id` INT NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id)
+);
