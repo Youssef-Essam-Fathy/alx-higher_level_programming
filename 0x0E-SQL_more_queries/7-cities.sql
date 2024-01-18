f182e2d8fc9748e255d7fc185ca34623b00767c8
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 16:13:00 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 CREATE table IF NOT EXISTS hbtn_0d_usa.cities
 (
-`id` INT PRIMARY KEY UNIQUE NOT NULL,
+`id` INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 `state_id` INT NOT NULL,
 FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 `name` VARCHAR(256) NOT NULL);
