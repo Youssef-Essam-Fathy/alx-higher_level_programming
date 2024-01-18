5fce75a7f3c627321456087e752033a5b59a29c8
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 13:41:57 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- creates the table id_not_null
-CREATE table IF NOT EXISTS id_not_null(`id` INT 1, `name` VARCHAR(256));
+CREATE table IF NOT EXISTS id_not_null(`id` INT DEFAULT 1, `name` VARCHAR(256));
