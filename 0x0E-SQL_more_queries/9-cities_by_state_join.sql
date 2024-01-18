ec97a6cb442f90e50aa1e01c9da66039c01b4b40
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-18 16:49:46 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities
-JOIN states ON cities.state_id=states.id
-ORDER BY cities.id;
+SELECT c.id, c.name, s.name 
+FROM cities AS c
+INNER JOIN states AS s ON cities.state_id=states.id
+ORDER BY c.id;
