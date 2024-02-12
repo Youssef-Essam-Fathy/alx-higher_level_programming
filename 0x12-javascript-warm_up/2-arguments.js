f7b9742269318ca75ae3d556f27982bf10dcb5ea
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-13 00:52:57 +0200

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length < 3) {
+const countargv = process.argv.length;
+if (countargv < 3) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (countargv === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
