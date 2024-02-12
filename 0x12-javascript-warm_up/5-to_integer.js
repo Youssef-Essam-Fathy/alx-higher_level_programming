4a43b6006c4260072e412bf6435a6d6aac796d5d
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-13 01:56:54 +0200

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const valargv = Number(process.argv[2]);
 if (isNaN(valargv)) {
-    console.log('Not a number');
-}
-else {
-    console.log('My number: ' + valargv);
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + valargv);
 }
