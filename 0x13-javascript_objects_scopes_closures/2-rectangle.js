e61b28cb565cd040f04f817e6655be9b7253f9d7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-13 20:42:36 +0200

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -8,8 +8,8 @@ class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
-    if (w <= 0 || h <= 0) {
-      Object.create(null);
+    if (w <= 0 && h <= 0) {
+      return {};
     }
   }
 }
