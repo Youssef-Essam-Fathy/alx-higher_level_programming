9cecd58dd32a0e2330786688ee17a38023f32838
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-13 02:22:31 +0200

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const l1 = 'C is fun'; const l2 = 'Python is cool'; const l3 = 'JavaScript is amazing';
+const arr = [l1, l2, l3];
+for (let i = 0; arr[i] !== undefined; i++) {
+    console.log(arr[i]);
+}
