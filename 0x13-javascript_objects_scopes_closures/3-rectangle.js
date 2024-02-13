5cd85fb6bdb314ddb83efb6b5df1eea33a001bd3
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-14 01:22:40 +0200

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/**
+ * A class defining a rectangle with constructor
+ * checking if the constructor params (w, h) are equal to 0
+ * or negative value then create empty object
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}
+module.exports = Rectangle;
