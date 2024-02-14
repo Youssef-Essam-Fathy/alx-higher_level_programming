cd319b91ebb9a7d9cc4f41c287932f510c14b685
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-14 20:15:40 +0200

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const li = [];
   let j = 0;
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     li[j] = list[i];
     j++;
   }
