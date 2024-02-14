e33eb9158b12ffba89a33d538892fd424bf3d2a3
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-14 02:46:16 +0200

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Sqre = require('./5-square.js');
+class Square extends Sqre {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        if (c !== undefined) {
+          line += 'C';
+        } else {
+          line += 'X';
+        }
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const li = [];
+  let j = 0;
+  for (let i = list.length - 1; i > 0; i--) {
+    li[j] = list[i];
+    j++;
+  }
+  return li;
+};
