b6f99c2a8c4a8eb9cae03309dc6392cddc05d808
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-16 21:00:51 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Importing the MySQLdb module"""
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host='localhost',
+        port='3306',
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        charset='utf-8'
+        )
+    cursor = connection.cursor()
+    try:
+        query = "SELECT * from states ORDER BY id ASC"
+        cursor.execute(query)
+        fetcher = cursor.fetchall()
+    except MySQLdb.Error:
+        try:
+            fetcher = ("MySQLdb Error")
+        except IndexError:
+            fetcher = ("MySQLdb Error - IndexError")
+
+    for i in fetcher:
+        print(i)
+    cursor.close()
+    connection.close()
