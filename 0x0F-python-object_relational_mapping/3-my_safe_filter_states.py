58ec208ce8e93dac3e79a98c4c862fd372b6f20a
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-17 21:59:22 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = %s\
+             ORDER BY ID ASC"
+    cur.execute(query, [argv[4]])
+    fetcher = cur.fetchall()
+    for i in fetcher:
+        print(i)
+    cur.close()
+    db.close()
