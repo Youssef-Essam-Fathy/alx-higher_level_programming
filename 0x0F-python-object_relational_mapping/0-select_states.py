fdb596e139bc093a845313145fce1124c2e5218a
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-17 12:20:01 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+"""a script that lists all states from the database"""
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
-if __name__ == '__main__':
-
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-
-    rows = cur.fetchall()
-    for i in rows:
+    query = "SELECT * FROM states ORDER BY INT ASC"
+    cur.execute(query)
+    fetcher = cur.fetchall()
+    for i in fetcher:
         print(i)
-    # Clean up process
     cur.close()
     db.close()
