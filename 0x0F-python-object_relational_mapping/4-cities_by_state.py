1ed76c9f7f3c4f07b57f2fc286a4a5970f3c38f0
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-18 16:33:55 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that lists all cities from the database """
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = conn.cursor()
+    query = "SELECT * FROM cities ORDER BY ID ASC"
+    cur.execute(query)
+    fetcher = cur.fetchall()
+    for i in fetcher:
+        print(i)
+    cur.close()
+    conn.close()
