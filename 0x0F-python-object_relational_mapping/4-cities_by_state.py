c78a81be0e34294dafbc012b3e3cc3096ad0e647
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-18 21:44:47 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,9 +4,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
+    cur = db.cursor()
     query = "SELECT cities.id, cities.name, states.name FROM cities\
              INNER JOIN states ON cities.state_id = states.id\
              ORDER BY cities.id ASC"
@@ -15,4 +15,4 @@ if __name__ == "__main":
     for i in fetcher:
         print(i)
     cur.close()
-    conn.close()
+    db.close()
