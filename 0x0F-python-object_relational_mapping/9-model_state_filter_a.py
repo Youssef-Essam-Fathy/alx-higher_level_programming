0ed8dbe34edfc3539f5bc1a8d107142cc0d0b875
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-19 02:25:05 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
 """
-import sys
+All states via SQLAlchemy
+"""
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+
+    session = Session(engine)
+    data = session.query(State).order_by(State.id)\
+                               .filter(State.name.like("%a%")).all()
+    for row in data:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
