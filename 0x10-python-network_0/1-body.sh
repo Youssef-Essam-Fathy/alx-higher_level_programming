207c5f0d2d5b26917e1f4151f01798a3ed44bf49
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-29 22:35:55 +0200

    0x10-python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that displays the size of the body of the response
-curl -X GET -s "$1" 
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the size of the body of the response
+curl -sL "$1"  
