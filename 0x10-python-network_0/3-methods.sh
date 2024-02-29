63361c281393066c7b4b6babb183af3b38586416
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 01:11:51 +0200

    0x10-python-network_0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i "Allow" | awk '{all=$0, sub("Allow: ", "", all), print all}'
+curl -sI "$1" | grep -i "Allow" | awk '{all=$0, sub("Allow: ", "", all); print all}'
