cf98c5a777fa5cd85a8c5e7f5196debc192b4e96
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 01:32:24 +0200

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that displays the size of the body of the response
+#Bash script that displays the size of the body of the response
 curl -sI "$1" | grep -i "Content-length" | awk '{print $2}'
