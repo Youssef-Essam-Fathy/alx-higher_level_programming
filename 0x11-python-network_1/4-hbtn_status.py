370cc72cbe7a5f8558ca75aba0aa25f0a700f6d5
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-02 00:10:15 +0200

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,10 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    try:
+        response = requests.get(url)
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
