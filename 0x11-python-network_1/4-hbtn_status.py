009b1a93fd343f7497c87a823a1d1289aaa559f3
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-02 00:05:31 +0200

    0x11-python-network_1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
