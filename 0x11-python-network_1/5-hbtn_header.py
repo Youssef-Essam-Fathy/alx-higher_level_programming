f29276325002bb56bcc4ff8b9bfce6cd3fdb588e
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-02 00:20:30 +0200

    0x11-python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, 
-sends a request to the URL and 
+Python script that takes in a URL,
+sends a request to the URL and
 displays the value of the variable X-Request-Id in the response header
 """
 import requests
