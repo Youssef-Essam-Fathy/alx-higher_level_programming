bec0eb72a81ef3bcb2504c5daaaef244cb2f71f2
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 02:07:48 +0200

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-def find_peak(list_of_integers):
-    """Find a peak in a list of integers"""
-    if list_of_integers:
-        low = 0
-        high = len(list_of_integers) - 1
+"""Finds a peak in a list of unsorted integers"""
 
-        while low < high:
-            mid = (low + high) // 2
-            if list_of_integers[mid] < list_of_integers[mid + 1]:
-                low = mid + 1
-            else:
-                high = mid
-        return list_of_integers[low]
-    else:
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+
+    if len(list_of_integers) == 0:
         return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        left, right = 0, len(list_of_integers) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if list_of_integers[mid] < list_of_integers[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return list_of_integers[left]
