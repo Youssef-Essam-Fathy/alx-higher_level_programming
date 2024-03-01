b81d512d137bd3720ef8a874be1de5183ac42119
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-02 01:08:36 +0200

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
 import requests
 from sys import argv
 
