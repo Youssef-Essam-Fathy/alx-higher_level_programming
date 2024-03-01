94f247103d8ca24bd185c7cdb412d3910a3eb3bb
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 14:26:19 +0200

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 import urllib.request
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
-        print('\t- utf8 content:', html.decode('utf-8'))
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode('utf-8'))
