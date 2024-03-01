7573c2b4306bec533ac6d3fd4cd918d97d542fde
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 14:27:27 +0200

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
