192e70055138c4d33bc47bd5b0f3cff413a31891
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-01 23:35:53 +0200

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL, 
-sends a request to the URL 
+a Python script that takes in a URL,
+sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 """
 import urllib.request
