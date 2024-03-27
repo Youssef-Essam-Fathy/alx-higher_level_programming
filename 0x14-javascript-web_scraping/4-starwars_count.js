55c96167bc26671abe49e48d2e20e258a3510b8f
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-27 03:44:14 +0200

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const swApi = process.argv[2];
+let num = 0;
+const charId = 18;
+
+request.get(swApi, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error:', `Received status code ${response.statusCode}`);
+    return;
+  }
+  const filmsData = JSON.parse(body).results;
+  filmsData.forEach((film) => {
+    film.characters.forEach((character) => {
+      if (character.includes(18)) {
+        num += 1;
+      }
+    });
+  });
+  console.log(num);
+});
