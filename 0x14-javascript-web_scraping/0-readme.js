b13bef4e2f023194c1ad7be40ae8a33c5b6650ab
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-27 02:52:46 +0200

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(`{ Error: ${err.message}\n    errno: ${err.errno},\n    code: '${err.code}',\n    syscall: '${err.syscall}',\n    path: '${err.path}' }`);
+    console.error(err);
     return;
   }
   console.log(data);
