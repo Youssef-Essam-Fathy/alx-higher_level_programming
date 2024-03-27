7f4a6a90d4b03931c17557ce92b2fcfa649e2202
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-27 03:08:15 +0200

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-const { error } = require('console');
 const request = require('request')
 
 const url = process.argv[2];
 
-request.get(url, (err, response, body) => {
+request.get(url, (err, response) => {
     if (err)
+    {
+        console.error(err);
+    }
+    console.log('code:', response.statusCode);
 });
